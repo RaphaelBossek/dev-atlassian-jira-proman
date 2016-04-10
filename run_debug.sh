6b53b288773f1cca03b4ebc81511ff9a01bcd8a0
Raphael Bossek <raphael.bossek@gmail.com>
2016-04-10 16:07:53 +0200

    Added support for external PostgreSQL data source including backup support after each run of run_debug.sh

--- run_debug.sh
+++ run_debug.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-exec atlas-debug --version 6.4.12 --product jira
+set -e
+instance="instance-postgres"
+#atlas-clean
+find target -name "proman-*" -exec rm -rf {} \;
+rm -rf target/classes
+atlas-debug --version 6.4.12 --product jira --instanceId $instance $* 2>&1|tee atlas-debug.log
+atlas-create-home-zip
+d=`date "+%Y%m%d-%H%M%S"`_$instance
+b=backup/$d
+mkdir -p $b
+mv atlas-debug.log $b
+mv target/$instance/generated-test-resources.zip $b
+cat >$b/dump_postgresql.sh <<__EOF__
+#!/bin/sh
+/Applications/pgAdmin3.app/Contents/SharedSupport/pg_dump --host macmini-rb --port 5432 --username "jira-amps" --role "jira-amps" --no-password  --format custom --blobs --encoding UTF8 --inserts --column-inserts --verbose --file "dump_postgresql.bin.sql" "jira-amps" 2>&1|tee dump_postgresql.log
+__EOF__
+(cd $b && sh dump_postgresql.sh)
+rm -f backup/last_$instance
+ln -s $d backup/last_$instance
+
